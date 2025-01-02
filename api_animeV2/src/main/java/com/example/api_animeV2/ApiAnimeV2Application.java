62c2caa5bad20504e56e5b03537af6e63afcd76f
Rafael Vaccaro <89113057+RafaelVaccaro@users.noreply.github.com>
2025-01-02 10:26:24 -0300

    start spring

---
+++ api_animeV2/src/main/java/com/example/api_animeV2/ApiAnimeV2Application.java
@@ -0,0 +1,13 @@
+package com.example.api_animeV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAnimeV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAnimeV2Application.class, args);
+	}
+
+}
---
+++ api_animeV2/src/test/java/com/example/api_animeV2/ApiAnimeV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_animeV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAnimeV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
