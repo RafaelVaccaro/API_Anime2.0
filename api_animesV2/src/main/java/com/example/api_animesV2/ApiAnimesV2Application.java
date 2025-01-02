7a3e2093dbb588980cba87e836e5437c4ba1574e
Rafael Vaccaro <89113057+RafaelVaccaro@users.noreply.github.com>
2025-01-02 10:01:06 -0300

    Spring Initizalizr OFICIAL

---
+++ api_animesV2/src/main/java/com/example/api_animesV2/ApiAnimesV2Application.java
@@ -0,0 +1,13 @@
+package com.example.api_animesV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAnimesV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAnimesV2Application.class, args);
+	}
+
+}
---
+++ api_animesV2/src/test/java/com/example/api_animesV2/ApiAnimesV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_animesV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAnimesV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
